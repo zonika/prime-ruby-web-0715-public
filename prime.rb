688ab39b564a034c25e7390b1c152fec1b715eeb
zonika <zchodosh@conncoll.edu>
2015-07-30 15:05:38 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    false
+  elsif num <= 3
+    true
+  elsif num % 2 ==0 || num % 3==0
+    false
+  else
+    i=5
+    while i*i <= num
+      if num % i == 0 or num % (i+2)==0
+        false
+      end
+      i+=6
+    end
+    true
+  end
+end
+      
+      
